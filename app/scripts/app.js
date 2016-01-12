1d6e2b03d1c68586c8693f412d12825ce014a5d2
Benjamin Doherty <bjd@bangpound.org>
2016-01-12 02:26:11 -0800

    initial commit.

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name kal3aTagsApp
+ * @description
+ * # kal3aTagsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('kal3aTagsApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kal3aTagsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the kal3aTagsApp
+ */
+angular.module('kal3aTagsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
