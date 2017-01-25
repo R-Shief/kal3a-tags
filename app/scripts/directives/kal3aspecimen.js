fdb463b623fb8912a142329ec37bf7893f9d23b7
Benjamin Doherty <bjd@bangpound.org>
2017-01-24 23:50:04 -0600

    add specimen component.

---
+++ app/scripts/directives/kal3aspecimen.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name kal3aTagsApp.directive:kal3aSpecimen
+ * @description
+ * # kal3aSpecimen
+ */
+angular.module('kal3aTagsApp')
+  .component('kal3aSpecimen', {
+      template: ['$element', function ($element) {
+        return [
+          '<div class="panel panel-default">',
+          '<div class="panel-body">',
+          '<pre><code><ng-transclude ng-transclude-slot="code"></ng-transclude></code></pre>',
+          '<div class="col-sm-10 col-sm-offset-1">',
+          '<ng-transclude ng-transclude-slot="preview"></ng-transclude>',
+          '</div>',
+          '</div>',
+          '</div>'
+        ].join('');
+      }],
+      transclude: {
+        code: 'code',
+        preview: 'preview'
+      }
+    }
+  );
